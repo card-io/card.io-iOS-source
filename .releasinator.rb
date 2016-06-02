587136ea3484d9d7c7c11438fe9a74a472b71bbc
Bhavani Bheemanadham <bbheemanadham@paypal.com>
2016-06-02 15:41:32 -0500

    cleaned up releasinator.rb

--- .releasinator.rb
+++ .releasinator.rb
@@ -34,18 +34,9 @@ def validate_paths
   @validator.validate_in_path("type mkvirtualenv")
 end
 
-def validate_virtualenvwrapper
-  if !File.exist?("/usr/local/bin/virtualenvwrapper.sh")
-    Printer.fail("mkvirtualenv is not setup in this system")
-    abort()
-  end 
-    Printer.success("mkvirtualenv setup properly in this system")
-end 
-
 # Custom validation methods.  Optional.
 configatron.custom_validation_methods = [
   method(:validate_paths),
-  method(:validate_virtualenvwrapper),
   method(:validate_podspec_version)
 ]
 
@@ -58,9 +49,6 @@ configatron.use_git_flow = true
 
 def build_cardio()
   CommandProcessor.command("VERSION=#{@current_release.version} ./releasinator.sh", live_output=true)
-  
-  # below step failing need to understand why it is failing 
-  #CommandProcessor.command("pod lib lint", live_output=true)
 end
 
 # The method that builds the sdk.  Required.
@@ -68,13 +56,6 @@ configatron.build_method = method(:build_cardio)
 
 # all the code moved to sumodule so this is just empty function 
 def publish_to_cocoapods(version)
-  #if File.exist?(File.expand_path("..", Dir.pwd)+"/card.io-iOS-SDK")
-  #  CommandProcessor.command("rm -rf "+File.expand_path("..", Dir.pwd)+"/card.io-iOS-SDK", live_output=true)
-  #end 
-  #CommandProcessor.command("git clone git@github.com:card-io/card.io-iOS-SDK.git "+ File.expand_path("..", Dir.pwd)+"/card.io-iOS-SDK", live_output=true)
-  #copy all file from relesae folder  to sdk folder 
-  
-  #CommandProcessor.command("python -u "+File.expand_path("pod trunk push CardIO.podspec ..", Dir.pwd)+"/card.io-iOS-SDK", live_output=true)
 end
 
 # The method that publishes the sdk to the package manager.  Required.
@@ -87,18 +68,9 @@ end
 # the method that waits for published artifact 
 configatron.wait_for_package_manager_method = method(:wait_for_cocoapods)
 
-def replace_version(new_tag)
-  replace_string("./SampleApp/build.gradle", "REPLACE_VERSION", "#{new_tag}")
-  replace_string("./README.md", "REPLACE_VERSION", "#{new_tag}")
-end
-
 def replace_gradle_package(filename, package_id, version)
   regex = /#{package_id}:\d\d*\.\d\d*\.\d\d*/
   replace_string(filename, regex, "#{package_id}:#{version}")
-end
-
-def replace_version_in_cordova(version)
-  replace_gradle_package("src/android/build.gradle", "io.card:android-sdk", version)
 end
 
 def add_new_line (filepath, location, new_content)
@@ -122,25 +94,19 @@ def update_release_notes(new_version)
   add_new_line("CHANGELOG.md", "-----", (@current_release.changelog.gsub! /^\*/,'* iOS:')+"\n\n")
 end
 
-def compile_sample_app()
-  Dir.chdir("SampleApp") do
-    CommandProcessor.command("gradlew clean assembleDebug", live_output=true)
-  end
-end
-
 def build_app()
   CommandProcessor.command("pod lib lint", live_output=true)
-  CommandProcessor.command("pod trunk push CardIO.podspec", live_output=true)
+ # CommandProcessor.command("pod trunk push CardIO.podspec", live_output=true)
 end 
 
 # Distribution GitHub repo if different from the source repo. Optional.
 configatron.downstream_repos = [
   DownstreamRepo.new(
     name="card.io-iOS-SDK",
-    url="git@github.com:sankarbhavanib/card.io-iOS-SDK.git",
+    url="git@github.com:card-io/card.io-iOS-SDK.git",
     branch="master",
     :release_to_github => true,
-    :full_file_sync => true,
+    :full_file_sync => false,
     :files_to_copy => [
       CopyFile.new("card.io_ios_sdk_#{podspec_version}*/*", ".", ".")
     ],
@@ -150,16 +116,15 @@ configatron.downstream_repos = [
   ),
   DownstreamRepo.new(
     name="card.io-Cordova-Plugin",
-    url="git@github.com:sankarbhavanib/card.io-Cordova-Plugin.git",
+    url="git@github.com:card-io/card.io-Cordova-Plugin.git",
     branch="master",
-    :full_file_sync => true,
+    :full_file_sync => false,
     :release_to_github => true,
     :new_branch_name => "ios-__VERSION__",
     :files_to_copy => [
-      CopyFile.new("card.io_ios_sdk_#{podspec_version}*/CardIO/*", ".", "src/ios/CardIO/")
+      CopyFile.new("card.io_ios_sdk_#{podspec_version}\*/CardIO/*", ".", "src/ios/CardIO")
     ],
     :post_copy_methods => [
-      method(:replace_version_in_cordova),
       method(:update_release_notes)
     ]
   )
